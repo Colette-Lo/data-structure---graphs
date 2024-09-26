3482701a7df0fed5a1efec5344e1609a3579e14e
Colette-Lo <clo11@srrcc.org.uk>
2024-09-26 12:10:26 +0100

    stack and dft

--- main.py
+++ main.py
@@ -20,16 +20,35 @@ def is_connected(new_list, node1, node2):
     if node1 in adj_list[node2]:
         print(True)
 
-stack_obg = Stack()
 
-current_node = "J"
 
-stack_obg.push(current_node)
-
-visited = []
-
-def dft(stack_obj):
+def dft(graph, start):
+    stack_obj = Stack()
+    # Start from J.
+    current_node = start
+    # Push current node to the stack.
+    stack_obj.push(current_node)
+    # A list of visited node.
+    visited = []
     while stack_obj.get_size() != 0:
-        visited.append(current_node)
-        for i in range(len(adj_list[current_node]) -1):
-            if adj_list[current_node][i] < adj_list[current_node][i+1]:
+        # Save the current node for later if new node is found.
+        temp_current = current_node
+
+        # Look through all the current node neighbours in alphabetical order.
+        for neighbour in sorted(graph[current_node]):
+            # If neighbour hasn't been visited, set current node to the neighbour.
+            if neighbour not in visited:
+                current_node = neighbour
+                stack_obj.push(current_node)
+                visited.append(current_node)
+                break
+
+        # If no change to the current node (no unvisited neighbours),
+        # pop the stack and set current node to be the returned node.
+        if current_node == temp_current:
+            current_node = stack_obj.pop()
+
+    print(visited)
+
+dft(adj_list, "J")
+
